3d140d01b20a009ec7fb833d00f8bdb34f4a967e
Mukesh Jha <mukeshmca.jha@gmail.com>
2020-10-19 17:07:39 +0530

    Completed human vs computer Creating Dart Stateful widget Use of Dart variable, function, expanded widget, flex properties, Dart Math library Random function, dart additional package import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -13,38 +14,98 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+/*Stateful widget - which is dynamic and may change on user interaction or logic*/
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int leftDiceNumber = 5;
+  int rightDiceNumber = 5;
+  String result;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: FlatButton(
-              onPressed: () {
-                print('Left Button Pressed');
-              },
-              child: new Image.asset('images/dice1.png'),
-            ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'YOU',
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      //setting variable state and updating UI
+                      randomizeDice();
+                    },
+                    child: new Image.asset('images/dice$leftDiceNumber.png'),
+                  ),
+                ],
+              ),
 
 //          child: Image(
 //            image: AssetImage('images/dice1.png'),
 //          ),
-          ),
-          Expanded(
-            flex: 1,
-            child: FlatButton(
-                onPressed: () {
-                  print('Right Button Pressed');
-                },
-                child: new Image.asset('images/dice1.png')),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'COMPUTER',
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
+                  ),
+                  FlatButton(
+                      onPressed: () {
+                        //do nothing
+                        //randomizeDice();
+                      },
+                      child:
+                          new Image.asset('images/dice$rightDiceNumber.png')),
+                ],
+              ),
 //          child: Image(
 //            image: AssetImage('images/dice1.png'),
 //          ),
-          )
-        ],
-      ),
+            )
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text(
+            result,
+            style: TextStyle(color: Colors.greenAccent, fontSize: 25.0),
+          ),
+        )
+      ],
     );
   }
+
+  void randomizeDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+
+      if (leftDiceNumber == rightDiceNumber) {
+        result = 'Congratulations!!! YOU WIN';
+      } else {
+        result = '';
+      }
+    });
+  }
 }
+
+/*Stateless widget - which is fixed and not going to change ever*/
+//class HomePage extends StatelessWidget {}
